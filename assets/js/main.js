8df7a32ec2061f40dab58e32c749a9c4f9891165
hyemin12 <khnm31@naver.com>
2021-06-04 17:53:13 +0900

    Start project

---
+++ assets/js/main.js
@@ -0,0 +1,167 @@
+// 검색창
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder','');
+});
+
+
+//스크롤 시 뱃지 제거 / top 버튼 보이기
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지 숨기기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    //top 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0,
+    });
+  }else{
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+    //top 버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100,
+    });
+  }
+}, 300)); 
+// _.throttle(함수, 시간)  throttle 함수를 사용하여 일정 시간에 한번씩만 실행되도록 제한
+// gsap.to(요소, 지속시간, 옵션);
+// javascript 애니메이션 소스
+
+//top 버튼 클릭 시 최상단으로 이동
+//사용하려면 ScrollToPlugin 필요
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0 //화면의 위치를 0px로 이동
+  })
+});
+
+
+//visual 이미지 순차적 애니메이션
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7 1.4 2.1 2.7
+    opacity: 1
+  });
+});
+
+
+//swiper - section notice/promotion
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true //사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl:'.promotion .swiper-prev',
+    nextEl:'.promotion .swiper-next'
+  }
+});
+
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl:'.awards .swiper-prev',
+    nextEl:'.awards .swiper-next'
+  }
+});
+
+
+//프로모션 토글
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click',function () {
+  isHidePromotion = !isHidePromotion //앞에 ! 붙이면 반대로 됨 ex) false -> true
+  if (isHidePromotion) {
+    //숨김 처리!
+    promotionEl.classList.add('hide'); //hide class 추가
+  }else{
+    //보임 처리!
+    promotionEl.classList.remove('hide'); //hide class 제거
+  };
+});
+
+
+//유튜브 아이콘 애니메이션(반복)
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, 
+    random(1.5, 2.5), 
+    { //옵션
+      y: size,
+      repeat: -1, //무한반복
+      yoyo: true, //한번 재생된 애니메이션 거꾸로 재생
+      ease: Power1.easeInOut, //gsap easing 옵션
+      delay: random(0, delay),
+    }
+  );
+};
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+// 스크롤 - 애니메이션 효과
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+
+//footer copyright 해당년도 출력
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); //현재 날짜 출력
